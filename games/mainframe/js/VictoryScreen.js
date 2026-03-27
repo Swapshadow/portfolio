689c3abbd3ccacfa4ae40914d7768f7943d85aca
Jean-Baptiste Terrazzoni <m9wrbmt2vj@privaterelay.appleid.com>
2026-03-27 13:09:29 +0100

    Add Mainframe game files

---
+++ games/mainframe/js/VictoryScreen.js
@@ -0,0 +1,123 @@
+Mainframe.VictoryScreen = function(game) {
+
+  this.elementLayer = null;
+  this.monitorLayer = null;
+
+  this.music = null;
+
+  this.dataValueText = null;
+  this.totalEarnedText = null;
+
+  this.dataValue = null;
+
+  this.nextCorpOption = null;
+  this.exitOption = null;
+  this.selectionIcon = null;
+  this.nextCorpSelected = null;
+
+  this.selectionSound = null;
+
+};
+
+Mainframe.VictoryScreen.prototype = {
+
+  create: function() {
+    this.elementLayer = this.game.add.group();
+    this.monitorLayer = this.game.add.group();
+
+    this.elementLayer.add(Mainframe.centreSprite(this.game.add.sprite(0, 70, 'atlas', 'Victory_Screen/run_successful.png'), this.game.width));
+    this.elementLayer.add(Mainframe.centreText(this.game.add.bitmapText(0, 175, 'green_font', Mainframe.corpName[0] + ' Data Obtained: ', 36), this.game.width));
+    this.elementLayer.add(Mainframe.centreSprite(this.game.add.sprite(0, 200, 'atlas', 'Victory_Screen/data_obtained_bar.png'), this.game.width));
+
+    this.elementLayer.add(this.game.add.bitmapText(269, 260, 'green_font', 'Run Chain Bonus: ', 28));
+    this.elementLayer.add(this.game.add.bitmapText(333, 285, 'green_font', 'Data Value: ', 28));
+    this.elementLayer.add(this.game.add.bitmapText(304, 310, 'green_font', 'Total Earned: ', 28));
+
+    this.elementLayer.add(this.game.add.bitmapText(480, 260, 'green_font', Mainframe.runChain.toFixed(1) + 'x', 28));
+
+    var percentage = 100 - ((3 - Mainframe.hackerProxies.length) * 20)
+    var value = percentage * 48;
+    value = value * Mainframe.runChain;
+    Mainframe.totalEarned += value;
+
+    this.dataValueText = this.game.add.bitmapText(480, 285, 'green_font', value + 'C', 28);
+    this.elementLayer.add(this.dataValueText);
+    this.totalEarnedText = this.game.add.bitmapText(480, 310, 'green_font', Mainframe.totalEarned + 'C', 28);
+    this.elementLayer.add(this.totalEarnedText);
+
+    var b = this.game.add.sprite(121, 217, 'atlas', 'Subroutines/General/trace_bar_full.png');
+    this.elementLayer.add(b)
+    b.width = 0;
+    b.height = 30;
+    b.alpha = 0.8;
+
+    this.game.add.tween(b).to({
+      width: (percentage / 100) * 718
+    }, 1000, Phaser.Easing.Linear.None, true, 0, 0, false);
+
+    this.nextCorpOption = this.game.add.bitmapText(0, 400, 'green_font', 'Next Corp', 30);
+    Mainframe.centreText(this.nextCorpOption, this.game.width);
+    this.elementLayer.add(this.nextCorpOption);
+
+    this.exitOption = this.game.add.bitmapText(0, 430, 'green_font', 'Exit', 30);
+    Mainframe.centreText(this.exitOption, this.game.width);
+    this.elementLayer.add(this.exitOption);
+
+    this.selectionIcon = this.game.add.bitmapText(200, 430, 'green_font', '>', 30);
+    this.elementLayer.add(this.selectionIcon);
+
+    var cursors = this.game.input.keyboard.createCursorKeys();
+
+    cursors.down.onDown.add(this.toggleSelection, this);
+    cursors.up.onDown.add(this.toggleSelection, this);
+
+    var space = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    space.onDown.add(this.select, this);
+
+    this.nextCorpSelected = true;
+
+    this.selectionSound = this.add.audio('selection');
+
+    this.repositionSelector();
+    var bg_flicker = this.game.add.sprite(0, 0, 'bg_flicker');
+    this.elementLayer.add(bg_flicker);
+    bg_flicker.animations.add('anim');
+    bg_flicker.animations.play('anim', 16, false);
+
+    this.monitorLayer.add(this.game.add.sprite(0, 0, 'atlas', 'General/monitor.png'));
+
+    Mainframe.mainMusic.volume = 0.5;
+  },
+
+  toggleSelection: function() {
+    this.selectionSound.play();
+    this.nextCorpSelected = !this.nextCorpSelected;
+    this.repositionSelector();
+  },
+
+  repositionSelector: function() {
+    if (this.nextCorpSelected) {
+      this.selectionIcon.y = this.nextCorpOption.y;
+      this.selectionIcon.x = this.nextCorpOption.x - 15;
+    } else {
+      this.selectionIcon.y = this.exitOption.y;
+      this.selectionIcon.x = this.exitOption.x - 15;
+    }
+  },
+
+  select: function() {
+    var bg_flicker = this.game.add.sprite(0, 0, 'bg_flicker_on');
+    this.elementLayer.add(bg_flicker);
+    bg_flicker.animations.add('anim');
+    bg_flicker.animations.play('anim', 16, false);
+    bg_flicker.events.onAnimationComplete.add(function() {
+      if (this.nextCorpSelected) {
+        Mainframe.runChain += 0.5;
+        this.state.start('MainScreen', true, false, true, null);
+      } else {
+        Mainframe.mainMusic.stop();
+        this.state.start('MainMenu');
+      }
+    }, this);
+  }
+};
